79bce2c28c759961ce6b3cd4f4bea36b24bc3d09
Doug Shook <dshook@wustl.edu>
2018-02-19 16:36:55 -0600

    assignment 6 starter

---
+++ src/assignment6/MsgReceiver.java
@@ -0,0 +1,24 @@
+package assignment6;
+
+import jssc.*;
+
+public class MsgReceiver {
+	
+	final private SerialComm port;
+	
+	public MsgReceiver(String portname) {
+		port = new SerialComm(portname);
+	}
+
+	public void run() {
+		// insert FSM code here to read msgs from port
+		// and write to console
+
+
+	}
+
+	public static void main(String[] args) {
+		MsgReceiver msgr = new MsgReceiver("COM4"); // Adjust this to be the right port for your machine
+		msgr.run();
+	}
+}
